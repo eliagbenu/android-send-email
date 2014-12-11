77041aad4ad539eb892bad646ff87de8aed4ce55
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2014-12-11 10:27:57 +0000

    did an app to send email

---
+++ app/src/main/java/com/eliagbenu/sendemail/MyActivity.java
@@ -0,0 +1,74 @@
+package com.eliagbenu.sendemail;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    Button buttonSend;
+    EditText textTo;
+    EditText textSubject;
+    EditText textMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textTo = (EditText) findViewById(R.id.editTextTo);
+        textSubject = (EditText) findViewById(R.id.editTextSubject);
+        textMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String to = textTo.getText().toString();
+                String subject = textSubject.getText().toString();
+                String message = textMessage.getText().toString();
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ to});
+                //email.putExtra(Intent.EXTRA_CC, new String[]{ to});
+                //email.putExtra(Intent.EXTRA_BCC, new String[]{to});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+
+                //need this to prompts email client only
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
